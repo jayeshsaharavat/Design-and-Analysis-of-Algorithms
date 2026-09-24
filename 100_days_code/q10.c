2d672700e507337619f14565bd107557c2bac294
Jayesh om Saharavat <jayeshsaharavat1@gmail.com>
2026-09-25 00:50:11 +0530

    Add program to print hollow square star pattern
    This program prompts the user for a value of n and prints a hollow square star pattern of size n.

---
+++ 100_days_code/q10.c
@@ -0,0 +1,29 @@
+// Write a generalized program to print a hollow square star pattern for any value of n.
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == n || j == 1 || j == n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
